46ebc7b419440526f62b0dfab01bf8a7221d55ea
Burns3y <21oburns@wakatipu.school.nz>
2025-06-03 13:05:42 +1200

    Balls drop

--- Scenes/Plinko/plinko.gd
+++ Scenes/Plinko/plinko.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+#Preloading balls
+var ball: PackedScene = preload("res://Scenes/Plinko/plinko_ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,6 +13,8 @@ func _process(delta):
 	pass
 
 
-
 func _on_ball_drop_pressed():
-	pass # Replace with function body.
+	var new_ball = ball.instantiate()
+	new_ball.name = "Ball"
+	$Balls.add_child(new_ball)
+	new_ball.position = $BallSpawnPoint.position + Vector2(1, 0)
