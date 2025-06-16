5b90c7795670ea67c3c0c61cd26687683475983f
Hunter <124668150+Strat15@users.noreply.github.com>
2025-06-17 11:38:05 +1200

    button work!!!!!
    blah me happy

---
+++ Area3D.gd
@@ -0,0 +1,8 @@
+extends Area3D
+
+func _input_event(_camera, event, _pos, _normal, _shape_idx):
+	if event is InputEventMouseButton and event.pressed:
+		if InputMap.action_has_event("left_click", event):
+			print("Clicked 3D button via Area3D!")
+			hide()  # Hides this Area3D and all its children (mesh, etc.)
+			$"../second_build".visible = true
