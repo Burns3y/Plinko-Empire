ae06985ea47cd0fb35c0f879c3493f6f734cba41
Burns3y <21oburns@wakatipu.school.nz>
2025-08-25 13:18:20 +1200

    Added bad popups

--- GlobalVariables.gd
+++ GlobalVariables.gd
@@ -1,8 +1,9 @@
 extends Node
 
-var coins: float = 100
+var coins: float = 1000
 var max_rows: int = 8
 var current_rows: int = 6
 var new_row_cost: int = 150
 var free_balls = []
 var income_per_minute: int = 2
+var animations_on: bool = true
