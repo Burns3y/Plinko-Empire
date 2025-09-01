ee0d2f778838d66df2cb501614f41529b7ddc111
Burns3y <21oburns@wakatipu.school.nz>
2025-09-01 19:49:31 +1200

    Added text for buttons

--- Area3D.gd
+++ Area3D.gd
@@ -1,8 +1,11 @@
 extends Area3D
 
+var clicked: bool = false
+
 func _input_event(_camera, event, _pos, _normal, _shape_idx):
 	if event is InputEventMouseButton and event.pressed:
 		if InputMap.action_has_event("left_click", event):
 			print("Clicked 3D button via Area3D!")
 			hide()  # Hides this Area3D and all its children (mesh, etc.)
 			$"../second_build".visible = true
+			clicked = true
